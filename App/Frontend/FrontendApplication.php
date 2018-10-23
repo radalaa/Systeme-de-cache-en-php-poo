1d985062db84067db86d331b617312e99f7fe295
radalaa <radalaa@gmx.fr>
2018-10-23 15:53:32 +0200

    Appler la methode verif_Cache_Accueil() qui prend en parametre le controlleur et l'action a excuter  -Rôle de la methode :  verifier la vie du fichier et si il existe, return le chemain du fichier

--- App/Frontend/FrontendApplication.php
+++ App/Frontend/FrontendApplication.php
@@ -18,23 +18,26 @@ class FrontendApplication extends Application
 
   public function run()
   {
-   
-
+  
   $controller = $this->getController();
   $Mymethod = $controller->execute();
-  $file = $controller->caheverif($Mymethod);
+  /////Début//////la mise en cache de la page d'accueil////////////////////////////
+  //instancier la class Cache
+  $cache = new Cache();
+  //appler la methode verif_Cache_Accueil() qui prend en parametre le controlleur et l'action a excuter
+  //Rôle de la methode :  verifier la vie du fichier et si il existe, return le chemain du fichier
+  $file = $cache->verif_Cache_Accueil($Mymethod,$controller);
   $existe = $file['existe'];
   $chemain = $file['chemain'];
-
-  if ($file['existe']) {
-    $this->httpResponse->sendCache($file['chemain']);
-  }else{
+  //si n'existe pas traitement normal, generer la page et creer un cache
+  if (!$file['existe']) {
     $this->httpResponse->setPage($controller->page());
     $this->httpResponse->send();
+  }else{
+    //appler la methode sendCache(), prend en parametre le chemain du fichier pour afficher le cache 
+    //qui est valide
+    $this->httpResponse->sendCache($file['chemain']);  
   }
-    
-
- 
-    
+  ///////Fin /////////////////////////////////////////////    
   }
 }
