484d198e8dec111ae2c7f9474bfee099820b39f8
radalaa <radalaa@gmx.fr>
2018-10-23 15:20:14 +0200

    La mise en place de deux mehode pour  la suppression manuelle des ressources en cache : si des ajouts, modification ou suppression ont été apportés, alors les ressources concernées doivent être supprimées du cache.

--- App/Backend/Modules/News/NewsController.php
+++ App/Backend/Modules/News/NewsController.php
@@ -8,23 +8,35 @@ use \Entity\Comment;
 use \FormBuilder\CommentFormBuilder;
 use \FormBuilder\NewsFormBuilder;
 use \OCFram\FormHandler;
+use \OCFram\Cache;
+
 
 class NewsController extends BackController
 {
   public function executeDelete(HTTPRequest $request)
   {
-    $newsId = $request->getData('id');
     
+    $newsId = $request->getData('id');
+    $this->deleteCacheNews($newsId);
+    
+
     $this->managers->getManagerOf('News')->delete($newsId);
     $this->managers->getManagerOf('Comments')->deleteFromNews($newsId);
 
     $this->app->user()->setFlash('La news a bien été supprimée !');
-
+        
     $this->app->httpResponse()->redirect('.');
   }
 
   public function executeDeleteComment(HTTPRequest $request)
   {
+//////////////////////////////////////////////////////////////////////
+    //Requette pour chercher l'id d'une news 
+    $newsId = $request->getData('id');
+    //@Methode deleteCacheComment() pour supprimer le cache
+    $this->deleteCacheComment($newsId);
+/////////////////////////////////////////////////////////////
+
     $this->managers->getManagerOf('Comments')->delete($request->getData('id'));
     
     $this->app->user()->setFlash('Le commentaire a bien été supprimé !');
@@ -58,6 +70,7 @@ class NewsController extends BackController
 
   public function executeUpdateComment(HTTPRequest $request)
   {
+   
     $this->page->addVar('title', 'Modification d\'un commentaire');
 
     if ($request->method() == 'POST')
@@ -82,8 +95,16 @@ class NewsController extends BackController
 
     if ($formHandler->process())
     {
-      $this->app->user()->setFlash('Le commentaire a bien été modifié');
+       
+      
+      
+        $this->app->user()->setFlash('Le commentaire a bien été modifié');
 
+/////////////////////////////////////////////////////////////
+        $newsId = $request->getData('id');
+        //@Methode deleteCacheComment() pour supprimer le cache
+        $this->deleteCacheComment($newsId);
+/////////////////////////////////////////////////////////////
       $this->app->httpResponse()->redirect('/admin/');
     }
 
@@ -128,10 +149,37 @@ class NewsController extends BackController
     if ($formHandler->process())
     {
       $this->app->user()->setFlash($news->isNew() ? 'La news a bien été ajoutée !' : 'La news a bien été modifiée !');
-      
-      $this->app->httpResponse()->redirect('/admin/');
+////////////////////////////////////////////////////////
+          //supprimer le cahe de la page d'accueil via la function deletecahe
+          $cache = new Cache();
+          $root = $cache->getChemain();
+          $file = $root.'/tmp/cache/views/index.php';
+          //appler @Methode pour supprimer le cache de la page d'accueil 
+          $cache->deletecache($file,'index.php');
+///////////////////////////////////////////////////////////
+          $this->app->httpResponse()->redirect('/admin/');
     }
 
     $this->page->addVar('form', $form->createView());
   }
+  public function deleteCacheComment($id){
+      //requette pour chercher le id news 
+      $comment = $this->managers->getManagerOf('Comments')->getUnique($id);
+      //supprimer le cache le cahe de la page d'accueil via la function deletecahe
+      $cache = new Cache();          
+      $cache->deleteCacheData($comment['news']);
+    return true;
+   
+  }
+////////////////////////////////////////////////////////////
+  public function deleteCacheNews($id){
+
+      $news = $this->managers->getManagerOf('News')->getUnique($id);
+      $cache = new Cache();          
+      $cache->deleteCacheData($news['id']);
+    return true;
+   
+  }
+/////////////////////////////////////////////////////////////
+
 }
